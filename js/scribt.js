5c641cdc9170fa9211babbccbff09793fdd84d81
Eng-MichaelAdel <engmichael89@gmail.com>
2025-06-25 11:51:22 +0300

    first commit

---
+++ js/scribt.js
@@ -0,0 +1,16 @@
+// ********** Asssigning Elements & values Functions for SignUp Page **********
+
+
+
+
+// ********** initializing users data  **********
+
+
+
+// ********** adding the input users data on submitting **********
+
+
+
+// ********** validations of Input user data for SignUp Page *******
+
+
